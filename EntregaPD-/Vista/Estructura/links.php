bf9fe50a9e700c013b13544579a3de7de44f1067
agusflores1 <agustinaafff@gmail.com>
2023-10-31 00:33:48 -0300

    intento subir TP5

--- EntregaPD-/Vista/Estructura/links.php
+++
@@ -1,6 +0,0 @@
-<meta charset=”utf8″ />
-<link rel="stylesheet" href="<?php echo BASE_URL ?>Vista/css/styles.css">
-<link rel="stylesheet" href="<?php echo BASE_URL ?>Vista/css/bootstrap-5.3.1-dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="<?php echo BASE_URL ?>Vista/css/bootstrap-icons-1.10.5/font/bootstrap-icons.css">
-
-</head>
